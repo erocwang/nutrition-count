dee6df6372f60d9cb9bcef523f8caa6dd7292060
Eric Wang <ew9@williams.edu>
2021-01-17 00:27:11 -0800

    first commit

--- App.js
+++ App.js
@@ -1,14 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import Amplify, {Auth} from 'aws-amplify';
+import {API, graphqlOperation} from 'aws-amplify'
+import awsconfig from './aws-exports'; 
+import {withAuthenticator} from 'aws-amplify-react-native';
+import {createTodo} from './graphql/mutations'
+import {listTodos} from './graphql/queries'
 
-export default function App() {
+Amplify.configure(awsconfig); 
+
+const initialState = {name: '', description: ''}
+
+function App()  {
   const [text1, setText1] = useState(''); 
   const [text2, setText2] = useState(''); 
+  const [formState, setFormsState] = useState(initialState)
+  const [todos, setTodos] = useState([])
 
+  useEffect(() => {
+    fetchTodos()
+  }, [])
+
+  function setInput(key,value) {
+    setFormsState({...formState, [key]:value})
+  }
+
+  async function fetchTodos() {
+    try {
+      const todoData = await API.graphql(graphqlOperation(listTodos))
+      const todos = todoData.data.listTodos.items
+      setTodos(todos)
+    } catch (err) { console.log('error fetching todos') }
+  }
+
+  async function addTodo() {
+    try {
+      const todo = { ...formState }
+      setTodos([...todos, todo])
+      setFormState(initialState)
+      await API.graphql(graphqlOperation(createTodo, {input: todo}))
+    } catch (err) {
+      console.log('error creating todo:', err)
+    }
+  }
+
+  async function signOut() {
+    try {
+      await Auth.signOut();
+    } catch (error) {
+      console.log('Error signing out: ', error);
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Fucbort</Text>
+      <TextInput
+        onChangeText={val => setInput('name', val)}
+        style={styles.input}
+        value={formState.name}
+        placeholder="Name"
+      />
+      <TextInput
+        onChangeText={val => setInput('description', val)}
+        style={styles.input}
+        value={formState.description}
+        placeholder="Description"
+      />
+      <Button title="Create Todo" onPress={addTodo} />
+      {
+        todos.map((todo, index) => (
+          <View key={todo.id ? todo.id : index} style={styles.todo}>
+            <Text style={styles.todoName}>{todo.name}</Text>
+            <Text>{todo.description}</Text>
+          </View>
+        ))
+      }
+      <Text>Balories</Text>
       <StatusBar style="auto" />
       <TextInput
         style={{height: 40}}
@@ -19,6 +86,7 @@ export default function App() {
       <Text style={{padding: 10, fontSize: 42}}>
         {text1.split(' ').map((word) => word && 'FUCKBORT').join(' ')}
       </Text>
+      <Text>Fats</Text> 
       <TextInput
         style={{height: 40}}
         placeholder="Type here to translate!"
@@ -28,9 +96,13 @@ export default function App() {
       <Text style={{padding: 10, fontSize: 42}}>
         {text2.split(' ').map((word) => word && 'FUCKBORT').join(' ')}
       </Text>
+      <Text>💙 + 💛 = React Native + Amplify </Text>
+      <Button title="Sign Out" color="tomato" onPress={signOut} />
     </View>
   );
 }
+
+export default withAuthenticator(App); 
 
 const styles = StyleSheet.create({
   container: {
@@ -39,4 +111,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  tood: {
+    marginBottom: 15
+  },
+  input: {
+    height:50,
+    backgroundColor: '#ddd',
+    marginBottom: 10, 
+    padding: 8
+  }, 
+  todoName: {
+    fontSize: 18
+  }
 });
